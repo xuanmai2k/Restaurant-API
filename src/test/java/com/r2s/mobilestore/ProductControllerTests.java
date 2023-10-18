4207e45e07490c3f9ab589a7234dff793dfa6daf
xuanmai2k <126083275+xuanmai2k@users.noreply.github.com>
2023-10-18 09:18:02 +0700

    author and junit test product

--- src/test/java/com/r2s/mobilestore/ProductControllerTests.java
+++ src/test/java/com/r2s/mobilestore/ProductControllerTests.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
